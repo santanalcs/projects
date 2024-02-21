309900ac2d37ee28701e8507f692d7c6ae4527cc
Luiz Santana <santanalcsgm@hotmail.com>
2024-02-21 11:17:22 -0300

    configuration initial - package.jso and tsconfig.json

---
+++ spgo/backend/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(express.static(path.join(__dirname, 'puclic')));
+server.use(express.json());
+server.use(express.urlencoded({extended:true}));
+
+server.listen(process.env.PORT, ()=>{
+    console.log(`Servidor rodando em: http://localhost:${process.env.PORT}`);
+});
